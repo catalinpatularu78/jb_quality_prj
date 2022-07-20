619ee9749232d02ca4192bee58e3d6abbb96b663
conniecons <alanconway91@gmail.com>
2022-07-20 11:54:21 +0100

    staticfiles

--- jb_quality/settings.py
+++ jb_quality/settings.py
@@ -164,4 +164,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-django_heroku.settings(locals())
+django_heroku.settings(locals() ,staticfiles=False)
