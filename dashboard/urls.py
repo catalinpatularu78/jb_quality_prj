60e71f815f6e189b2568014d3ee90e201da6308f
conniecons <alanconway91@gmail.com>
2022-07-21 08:54:45 +0100

    .

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -28,9 +28,9 @@ urlpatterns = [
     path('filter_dashboard/', FilterDashboardPage.as_view(),  name= 'filter_dashboard'),
     
     path('dashboard/record_create/', RecordCreatePage.as_view(),  name= 'record_create'),
-    path("dashboard/record_details/<uuid:pk>/", RecordDetailPage.as_view(), name="record_detail"),
-    path('dashboard/record_update/<uuid:pk>/', RecordUpdatePage.as_view(),  name= 'record_update'),
-    path('dashboard/record_delete/<uuid:pk>', RecordDeletePage.as_view(),  name= 'record_delete'),
+    path("dashboard/record_details/<str:pk>/", RecordDetailPage.as_view(), name="record_detail"),
+    path('dashboard/record_update/<str:pk>/', RecordUpdatePage.as_view(),  name= 'record_update'),
+    path('dashboard/record_delete/<str:pk>', RecordDeletePage.as_view(),  name= 'record_delete'),
 
     path('dashboard/production_issue_update/', IssueFormPage.as_view(),  name= 'production_issue_update'),
     
