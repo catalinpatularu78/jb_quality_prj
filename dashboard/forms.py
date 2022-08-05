864fec602804e841cdb094a30059ae52b5317960
Conor McN <conorm967@gmail.com>
2022-08-05 16:46:03 +0100

    most recent changes

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -77,7 +77,7 @@ class RecordForm(ModelForm):
             "estimated_completion_time" : "Estimated Completion Time", 
             "advice_number" : "Advice Number",
             "downtime_time" : "Downtime",
-            "the_subject_responsible" : "Person Responsible", 
+            "the_subject_responsible" : "Person/Company Responsible", 
             "supervisor" : "Supervisor",
             "description" : "Description", 
             "root_cause" : "Root Cause", 
