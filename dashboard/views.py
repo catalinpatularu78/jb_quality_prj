eeeece47bd57852b8882aedbb0312f4ad3cdc1a5
Conor McN <conorm967@gmail.com>
2022-08-17 14:01:13 +0100

    Image upload fixes

--- dashboard/views.py
+++ dashboard/views.py
@@ -360,14 +360,14 @@ class RecordUpdatePage(StaffMemberRequiredMixin, LoginRequiredMixin , UpdateView
             f.save()        
 
             if(d.image_set.first()): #if images are present in the set
-                DashboardModel.objects.get(image=d.image_set.first()).delete() # delete the previous images before updating         
+                DashboardModel.objects.get(image=d.image_set.first()).delete() # delete the previous images before updating     
+                for i in files:
+                    Image.objects.create(project=f, image=i)    
             else:
-                pass
-                
-            for i in files:
-                Image.objects.create(project=f, image=i)
-                  
-            DashboardModel.objects.first().delete()         
+                for i in files:
+                    Image.objects.create(project=f, image=i)   
+                DashboardModel.objects.first().delete()   
+    
             messages.success(request, "New image updated")
 
             return self.form_valid(form)
